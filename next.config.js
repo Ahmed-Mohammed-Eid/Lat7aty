7049ce3535740b78743d5958bd62166e7f2199fb
Ahmed-Mohammed-Badawi <AhmedIsmail73990@azhar.edu.eg>
2024-04-03 16:28:46 +0200

    ADD DUMMY GOOGLE MAPS API KEY FOR TESTING

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    GOOGLE_MAPS_API_KEY: "",
+    GOOGLE_MAPS_API_KEY: "AIzaSyAQq1-2Hm_rdN0qKzPEwwzO4mWaVoJLNDQ",
   },
   images: {
     domains: ['picsum.photos', "unsplash.com", "images.unsplash.com", "source.unsplash.com", "images.pexels.com", "mandoob.kportals.net", "mandoobku.com", "api.lathaty.com", "lathaty.com"],
